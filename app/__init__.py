5e6630796c1cd4c033f9eb1fa8b0782e7a08a8f8
max <max.avtenyuk@gmail.com>
2015-11-03 13:55:53 +0200

    add flask-triangle

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.triangle import Triangle
 
 app = Flask(__name__)
 app.config.from_object('config')
+Triangle(app)
+
+db = SQLAlchemy(app)
+
 
 from app import views
--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+SQLALCHEMY_DATABASE_URI = "postgresql://usertodo:123456@localhost/supertodo"
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
