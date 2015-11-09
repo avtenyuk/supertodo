15f1dd23af4596ea33eed2306d2f89ce0c5de8ca
san4ezy <san4ezy@gmail.com>
2015-11-09 16:52:21 +0200

    test

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -17,43 +17,6 @@ var superTodo = angular.module('superTodo', []);
  	]
  );
 
-//superTodo.controller('StickersListCtrl', function($scope){
-//	$scope.stickers = [
-//		{
-//			"title": "The first sticker",
-//			"memo": "Hello! If you can read me, then the developers do something :)",
-//			"created": "2015-11-03",
-//			"folder": "root"
-//		},
-//		{
-//			"title": "I`m a sticker number two!", //And you must to do!
-//			"memo": "Memo writes here",
-//			"created": "2015-11-01",
-//			"folder": "root"
-//		},
-//		{
-//			"title": "St#3",
-//			"memo": "If you wont make the fucking report, boss will cut your balls!!!",
-//			"created": "2015-11-02",
-//			"folder": "Work"
-//		},
-//		{
-//			"title": "Product list",
-//			"memo": "",
-//			"created": "2015-10-30",
-//			"folder": "Market"
-//		},
-//		{
-//			"title": "Must do",
-//			"memo": "",
-//			"created": "2015-10-01",
-//			"folder": "root"
-//		}
-//	]
-//
-//	$scope.orderProp = '!created';
-//});
-
 superTodo.controller('FoldersListCtrl', function($scope){
 	$scope.folders = [
 		{
