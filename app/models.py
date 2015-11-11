58dab57d53da32557bc1b6eaffc1d08af34a7f5e
max <max.avtenyuk@gmail.com>
2015-11-11 19:18:19 +0200

    add login form and api of task

--- app/models.py
+++ app/models.py
@@ -71,3 +71,13 @@ class Task(db.Model):
 
     def as_json(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    password = db.Column(db.String())
+
+    def __str__(self):
+        return self.nickname
--- sticker_generator.sh
+++ sticker_generator.sh
@@ -8,10 +8,11 @@ min_num=1
 i=$min_num
 while [ $i -le $big_number ];
 do
-        curl -i -H "Content-Type: application/json" -X POST -d '{"title":"title new sticker", "memo":"text genereted", "folder_id":1}' http://127.0.0.1:5000/api/v1.1/sticker
+        curl -i -H "Content-Type: application/json" -X POST -d '{"title":"title new sticker", "memo":"text genereted", "folder_id":1}' http://127.0.0.1:5000/api/sticker
         echo $i
         i=$[i+1]
 done
 
 # curl -X "DELETE" http://127.0.0.1:5000/api/sticker/10265
 # curl -i -H "Content-Type: application/json" -X POST -d '{"status": false, "text":"to fo something", "sticker_id": 10280}' http://127.0.0.1:5000/api/task
+# curl -i -H "Content-Type: application/json" -X PUT -d '{"status": "true"}' http://127.0.0.1:5000/api/task/10
